f2044d482164aef57db8bb541f71f7e3fd541352
edris <edris.qeshm2@gmail.com>
2024-05-16 13:19:22 +0330

    fix: add counter functionality #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,64 @@
 const modal = document.querySelector('.modal');
+const modalTitle = document.querySelector('#modal-title');
+const dhikrTitle = document.querySelector('#dhikr-title');
+const dhikrText = document.querySelector('#dhikr-text');
+const dhikrTranslation = document.querySelector('#dhikr-translation');
+const counterButton = document.querySelector('.counter-button');
 
-const openModal = () => {
-  modal.classList.remove('hidden');
+let openedCollection = null;
+let openedDhikr = null;
+let counter = 0;
+let dhikrIndex = 0;
+
+const injectCollectionDataIntoModal = (collection) => {
+    updateCounterLabelValue();
+    modalTitle.textContent = collection.name;
+    injectDhikrDataIntoModal();
+}
+
+const injectDhikrDataIntoModal = () => {
+    dhikrTitle.textContent = openedDhikr.name;
+    dhikrText.textContent = openedDhikr.text;
+    dhikrTranslation.textContent = openedDhikr.translation;
+}
+
+const count = () => {
+    counter++;
+    updateCounterLabelValue();
+    if (counter >= openedDhikr.count) {
+        gotoNextDhikr();
+    }
+}
+
+const thereIsANextDhikr = () => {
+    return openedCollection.adhkar[dhikrIndex + 1] !== undefined;
+}
+
+const gotoNextDhikr = () => {
+    counter = 0;
+    if (thereIsANextDhikr()){
+        openedDhikr = openedCollection.adhkar[++dhikrIndex];
+        updateCounterLabelValue();
+        injectDhikrDataIntoModal();
+    }
+    else{
+        closeModal();
+    }
+}
+
+const updateCounterLabelValue = () => {
+    counterButton.textContent = counter;
+}
+
+const openModal = (collection) => {
+    openedCollection = collection;
+    openedDhikr = collection.adhkar[dhikrIndex];
+    counter = 0;
+    injectCollectionDataIntoModal(collection);
+    modal.classList.remove('hidden');
 };
 
 const closeModal = () => {
-  modal.classList.add('hidden');
+    modal.classList.add('hidden');
 };
 
