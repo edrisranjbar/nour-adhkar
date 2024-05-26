15a53f3365ce25f9fc83e5254e768c117f2ad787
edris <edris.qeshm2@gmail.com>
2024-05-26 07:07:17 +0330

    enhance: seperated data and models and added night collection

---
+++ assets/js/collection.js
@@ -0,0 +1,6 @@
+class Collection {
+    constructor(name, adhkar) {
+        this.name = name;
+        this.adhkar = adhkar;
+    }
+}
---
+++ assets/js/dhikr.js
@@ -0,0 +1,9 @@
+class Dhikr {
+    constructor(name, prefix, text, translation, count) {
+        this.name = name;
+        this.prefix = prefix;
+        this.text = text;
+        this.translation = translation;
+        this.count = count;
+    }
+}
