592715f04c2a02350d986fe6fdd4444d6b9b30f4
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2024-09-10 10:32:22 +0330

    fix: counter button

--- Nour Adhkar/src/components/CounterButton.vue
+++ Nour Adhkar/src/components/CounterButton.vue
@@ -1,50 +1,62 @@
 <style scoped>
-    .counter-button {
-        width: max(280px, 70%);
-        aspect-ratio: 1/1;
-        background-color: #9C8466;
-        background-image: url("../assets/images/counterButton.svg");
-        background-repeat: no-repeat;
-        background-size: cover;
-        border-radius: 50%;
-        position: relative;
-        z-index: 1;
-        overflow: hidden;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 32px;
-        cursor: pointer;
-    }
+.counter-button {
+    width: max(280px, 70%);
+    aspect-ratio: 1/1;
+    background-color: #9C8466;
+    background-image: url("../assets/images/counterButton.svg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 50%;
+    position: relative;
+    z-index: 1;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 32px;
+    cursor: pointer;
+}
 
-    .counter-label {
-        font-weight: 700;
-        font-size: 96px;
-        color: #ffffff;
-        text-align: center;
-        user-select: none;
-    }
-
+.counter-label {
+    font-weight: 700;
+    font-size: 96px;
+    color: #ffffff;
+    text-align: center;
+    user-select: none;
+}
 </style>
 
 <template>
-    <div class="counter-button" @click="counter++">
+    <div class="counter-button" @click="this.count()">
         <span class="counter-label">{{ counter }}</span>
     </div>
 </template>
 
 <script>
+import tapSound from "@/assets/audios/click.mp3"
+
 export default {
     data() {
         return {
             counter: 0,
+            tapSoundAudioPath: tapSound,
         };
     },
-    handleKeydown: function (event) {
-        if (event.code === 'Space') {
+    methods: {
+        count: function () {
             this.counter++;
+            this.playAudio(this.tapSoundAudioPath);
+        },
+        playAudio: function (audioPath) {
+            const audio = new Audio(audioPath);
+            audio.play();
+        },
+        handleKeydown: function (event) {
+            if (event.code === 'Space') {
+                this.count();
+            }
         }
     },
     mounted() {
