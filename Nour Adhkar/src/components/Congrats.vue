b34e10ac6383867bd3f66eb1710582c4e0420af1
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2025-02-27 10:02:50 +0330

    fix styles

--- Nour Adhkar/src/components/Congrats.vue
+++ Nour Adhkar/src/components/Congrats.vue
@@ -10,6 +10,7 @@ a {
     bottom: 0;
     left: 0;
     right: 0;
+    width: min(100%, 600px);
     max-width: calc(100vw - 32px);
     background-color: rgba(255, 255, 255);
     border-radius: 16px;
