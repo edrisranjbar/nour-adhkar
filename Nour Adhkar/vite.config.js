29e86c58a97d6b2364052389434b98d30b54d3f3
Edris <edris.qeshm2@gmail.com>
2025-04-27 05:06:29 +0330

    chore: update Vite configuration to enable HMR for production with specified host and port

--- Nour Adhkar/vite.config.js
+++ Nour Adhkar/vite.config.js
@@ -15,14 +15,9 @@ export default defineConfig({
   server: {
     cors: true,
     allowedHosts: ['adhkar.ir', 'www.adhkar.ir', 'localhost', '127.0.0.1', '85.198.10.144'],
-  },
-  // Disable HMR in production
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
+    hmr: {
+      host: 'adhkar.ir',
+      port: 8888,
     },
-  }
+  },
 })
