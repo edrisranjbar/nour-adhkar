2c8cf41f277210d0e5b74a971125d9cf2ce65cd3
Edris <edris.qeshm2@gmail.com>
2025-05-03 21:31:51 +0330

    Fix pattern.svg URL references to use proper Vue @alias format

--- Nour Adhkar/src/components/CategoryCard.vue
+++ Nour Adhkar/src/components/CategoryCard.vue
@@ -35,7 +35,7 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background: url('src/assets/images/pattern.svg') no-repeat;
+  background: url('@/assets/images/pattern.svg') no-repeat;
   background-size: cover;
   opacity: 0.1;
   z-index: 1;
